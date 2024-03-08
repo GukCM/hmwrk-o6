77b600fb72b44838d0108ed791d97c8c2f165e35
GukCM <gusavocod@gmail.com>
2024-03-07 20:25:01 -0600

    Final

---
+++ index.js
@@ -0,0 +1,19 @@
+let number = 32;
+let float = 35.3 
+let char = "Hola, mundo!"; 
+let boolean = true; 
+let array = [11, 22, 33, 44, 56]; 
+let object = { nombre: "Gustavo", edad: 21 }; 
+
+console.log(
+    "Número: ", number,
+    "Número float o decimal: ", float,
+    "char: ", char,
+    "boolean: ", boolean,
+    "array: ", array,
+    "object: ", object
+    );
+
+
+
+// Path: index.js
